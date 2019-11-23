0f7bb9be64d4e757efa8dd2a6725e93fad343e52
Hantasmagoria <30933301+Hantasmagoria@users.noreply.github.com>
2019-11-23 16:20:50 +0800

    imported from old repo

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+client.config = config;
+
+client.on("message", message => {
+  let prefix = client.config.prefix;
+  let args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  let command = args.shift().toLowerCase();
+
+  if (command === "quote") {
+    console.log(
+      "Initiated Quote command. \n",
+      `${args[0] === "?" ? "" : "Quoting messageId " + args[0]} \n`,
+      `sending to channel ${message.channel.name} in ${message.guild}`
+    );
+    if (args[0] === "?") {
+      console.log("Quote command help requested at " + message.channel);
+      message.channel.send("Example usage: ```>quote 645305062230589450 ```");
+    } else if (typeof parseInt(args[0]) === "number") {
+      message.channel
+        .fetchMessage(args[0])
+        .then(message =>
+          message.channel.send(
+            new Discord.RichEmbed()
+              .setTitle("Quote from " + message.author.username)
+              .setAuthor(message.author.username, message.author.avatarURL)
+              .setColor(0xc736e5)
+              .setDescription(message.content)
+              .setTimestamp(message.createdTimestamp)
+              .setURL(message.url)
+          )
+        )
+        .catch(console.error);
+    }
+  }
+});
+
+client
+  .login(`${process.env.BOT_TOKEN}`)
+  .then(console.log("I am ready!"))
+  .catch(console.error); //BOT_TOKEN set in heroku as a config var, setting already done. I think.
